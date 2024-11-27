75d840dc19696645d8ac40ad82f8877d79770e26
luandcille <lucille.vigne@gmail.com>
2024-11-27 23:05:36 +0100

    update metadata

---
+++ rainbow/components/meta.py
@@ -0,0 +1,20 @@
+import reflex as rx
+
+meta_tags = [
+    # HTML Meta Tags
+    {"name": "application-name", "content": "Lucille Vigné"},
+    {
+        "name": "keywords",
+        "content": "reflex, python, web apps, lucille vigné, aesthetic, notion",
+    },
+    {
+        "name": "description",
+        "content": "Beneath the Rainbow Sky",
+    },
+]
+
+def favicons_links() -> list[rx.Component]:
+    return [
+        rx.el.link(rel="icon", type="image/png", sizes="32x32", href="images/lucille_logo_32.png"),
+        rx.el.link(rel="icon", type="image/png", sizes="16x16", href="images/lucille_logo_16.png"),
+    ]
--- rainbow/rainbow.py
+++ rainbow/rainbow.py
@@ -5,14 +5,17 @@ from rainbow.sections.header import header
 from rainbow.sections.body import body
 from rainbow.sections.footer import footer
 from rainbow.components.badge import badge
+from rainbow.components.meta import meta_tags, favicons_links
 
 app = rx.App(
     stylesheets = 
-        ["styles/style.css", 
+        ["fashion/main.css", 
          "https://fonts.googleapis.com/css2?family=Didact+Gothic&display=swap"
         ], 
+    head_components=favicons_links()
 )
 
+@rx.page(route="/", title="Lucille Vigné · Beneath the Rainbow Sky", meta=meta_tags)
 def index() -> rx.Component:
     return rx.container(
         header(),
@@ -27,5 +30,4 @@ def index() -> rx.Component:
             )
         },
 ),
-    
 app.add_page(index)
